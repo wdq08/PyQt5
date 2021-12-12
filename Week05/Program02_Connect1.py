fce545706ff993e2572b231d86fb3870db54e9d4
wdq <86144485+wdq08@users.noreply.github.com>
2021-12-12 10:47:23 +0800

    Update

---
+++ Week05/Program02_Connect1.py
@@ -0,0 +1,75 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from Program02_Connect2 import Ui_SecondWindow 
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        self.IsOpened = False
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(400, 300)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget, clicked = lambda: self.clicked())
+        self.pushButton.setGeometry(QtCore.QRect(30, 100, 340, 60))
+        font = QtGui.QFont()
+        font.setPointSize(46)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget, clicked = lambda: self.openWindow(self))
+        self.pushButton_2.setGeometry(QtCore.QRect(30, 190, 340, 50))
+        font = QtGui.QFont()
+        font.setPointSize(35)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(30, 30, 340, 50))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 400, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Submit"))
+        self.pushButton_2.setText(_translate("MainWindow", "Open Window"))
+
+    def openWindow(self,self_2):
+        self_2.IsOpened = True
+        # Open the second window
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_SecondWindow()
+        self.ui.setupUi(self.window)
+        self.window.show()
+        MainWindow.hide()
+
+    def clicked(self):
+        if self.IsOpened:
+            # I am using this if statement because you must assign the second window before you use any thing in it
+            self.input = self.lineEdit.text()
+            # Assign input to the next window's label
+            self.ui.label.setText(self.input)
+        else:
+            self.lineEdit.setText("Please open the other window first!")
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ Week05/Program02_Connect2.py
@@ -0,0 +1,63 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_SecondWindow(object):
+    def setupUi(self, SecondWindow):
+        SecondWindow.setObjectName("SecondWindow")
+        SecondWindow.resize(500, 200)
+        self.centralwidget = QtWidgets.QWidget(SecondWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(30, 30, 440, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(30, 100, 121, 50))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(170, 100, 141, 51))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(330, 100, 151, 51))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.pushButton_3.setFont(font)
+        self.pushButton_3.setObjectName("pushButton_3")
+        SecondWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(SecondWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 18))
+        self.menubar.setObjectName("menubar")
+        SecondWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(SecondWindow)
+        self.statusbar.setObjectName("statusbar")
+        SecondWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(SecondWindow)
+        QtCore.QMetaObject.connectSlotsByName(SecondWindow)
+
+    def retranslateUi(self, SecondWindow):
+        _translate = QtCore.QCoreApplication.translate
+        SecondWindow.setWindowTitle(_translate("SecondWindow", "MainWindow"))
+        self.label.setText(_translate("SecondWindow", "Type something in the other window"))
+        self.pushButton.setText(_translate("SecondWindow", "Hide main window"))
+        self.pushButton_2.setText(_translate("SecondWindow", "Show Main Window"))
+        self.pushButton_3.setText(_translate("SecondWindow", "Hide this window"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    SecondWindow = QtWidgets.QMainWindow()
+    ui = Ui_SecondWindow()
+    ui.setupUi(SecondWindow)
+    SecondWindow.show()
+    sys.exit(app.exec_())
